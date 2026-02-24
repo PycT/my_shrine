dca52959747196745ef020f9e9de3a0aed567b4d
PycT <rustem.zakiev@gmail.com>
2026-02-24 22:04:29 +0100

    feat: add shrine switch buttons in responsive Wrap layout

--- my_shrine/lib/views/tracker_view.dart
+++ my_shrine/lib/views/tracker_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:my_shrine/data/app_styles.dart';
 import 'package:my_shrine/widgets/authentication/auth_gate.dart';
 import 'package:my_shrine/widgets/common_app_bar.dart';
+import 'package:my_shrine/widgets/shrine_switch_widget.dart';
 import 'package:my_shrine/widgets/tracker_toggle_widget.dart';
 import 'package:my_shrine/entities/shrine.dart';
 
@@ -16,6 +18,10 @@ class TrackerViewPage extends StatelessWidget {
 class TrackerView extends StatelessWidget {
   const TrackerView({super.key});
 
+  List<Widget> _shrineSwitches(List<Shrine> shrines) {
+    return shrines.map((shrine) => ShrineSwitchWidget(shrine: shrine)).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     final shrines = [
@@ -26,7 +32,22 @@ class TrackerView extends StatelessWidget {
     return Scaffold(
       appBar: CommonAppBar(title: "It is a great day!"),
       body: SafeArea(
-        child: Center(child: Column(children: [TrackerToggleWidget()])),
+        child: Center(
+          child: Column(
+            children: [
+              TrackerToggleWidget(),
+              SizedBox(height: AppStyles.verticalSeparatorHeight),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Wrap(
+                  spacing: AppStyles.verticalSeparatorHeight,
+                  runSpacing: AppStyles.verticalSeparatorHeight,
+                  children: _shrineSwitches(shrines),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
